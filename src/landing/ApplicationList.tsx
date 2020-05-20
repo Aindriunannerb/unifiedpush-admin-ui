427aef87f2a30bab09b98301ece23d47c3e3358e
Andrew Brennan <andrew-brennan@live.com>
2020-05-20 13:11:10 +0100

    Aligns buttons to the righ, Buttons hidden unless hovered over

--- src/landing/ApplicationList.tsx
+++ src/landing/ApplicationList.tsx
@@ -88,34 +88,6 @@ export class ApplicationList extends Component<Props, State> {
                         icon={'fa fa-mobile'}
                       />
                     </ListItem>
-                    <ListItem>
-                      <Button
-                        variant="secondary"
-                        icon={<EditIcon />}
-                        onClick={() =>
-                          this.setState({
-                            deleteApplicationPage: true,
-                            selectedApp: app,
-                          })
-                        }
-                      >
-                        <EditIcon />
-                      </Button>
-                    </ListItem>
-                    <ListItem>
-                      <Button
-                        variant="danger"
-                        icon={TrashIcon}
-                        onClick={() =>
-                          this.setState({
-                            deleteApplicationPage: true,
-                            selectedApp: app,
-                          })
-                        }
-                      >
-                        <TrashIcon />
-                      </Button>
-                    </ListItem>
                   </List>
                 </div>
               </DataListCell>,
